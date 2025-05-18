1ef12a87cae1aa22945993c3530cf156315ee7da
Farhaan4 <127176994+Farhaan4@users.noreply.github.com>
2025-05-18 19:36:05 +0700

    commit

--- layouts/default.vue
+++ layouts/default.vue
@@ -9,7 +9,7 @@
 </template>
 
 <script setup>
-import Navbar from '@/components/navbar.vue'
+import Navbar from '@/components/Navbar.vue'
 /*import Footer from '@/components/footer.vue'*/
 </script>
 <style scoped>
